347d33096e2e43ed81dc1def2e93faf68da0b526
Tanweer.Zaman <tzaman536@gmail.com>
2016-01-10 11:40:21 -0500

    sw

---
+++ SimplexWebApplication/SimplexWeb/Controllers/SoftwareConsultingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simplex.Controllers
+{
+    public class SoftwareConsultingController : Controller
+    {
+        //
+        // GET: /SoftwareConsulting/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
