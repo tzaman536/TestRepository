8e18250d3f7634eb2f364954bf768e79e181b6b5
Tanweer.Zaman <tzaman536@gmail.com>
2016-01-23 08:58:40 -0500

    Adding code for AMZ Wholesale

---
+++ AmzWholeSaleWeb/AmzWholeSaleWeb/Controllers/AmzHomeController.cs
@@ -0,0 +1,59 @@
+﻿using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmzWholeSaleWeb.Controllers
+{
+    public class AmzHomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(GetProductsLocal());
+        }
+
+        public ActionResult Products_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetProductsLocal().ToDataSourceResult(request));
+        }
+
+
+        private static IEnumerable<ProductViewModel> GetProductsLocal()
+        {
+            List<ProductViewModel> result = new List<ProductViewModel>();
+            for (int i = 1; i < 15; i++)
+            {
+                result.Add(new ProductViewModel
+                {
+                    ProductID = i,
+                    ProductName = string.Format("Name {0}", i),
+                    UnitPrice = 10,
+                    UnitsInStock = 50,
+                    UnitsOnOrder = 10,
+                    Discontinued = false,
+                    LastSupply = DateTime.Today
+                });
+            }
+
+            return result;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AmzWholeSaleWeb/AmzWholeSaleWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AmzWholeSaleWeb.Startup))]
+namespace AmzWholeSaleWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
