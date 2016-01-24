1f9ed23d33eee42c190f7bf727c4898b965abbb4
Tanweer.Zaman <tzaman536@gmail.com>
2016-01-24 10:59:54 -0500

    sw

--- AmzWholeSaleWeb/AmzBL/Products/AmzProductData.cs
+++ AmzWholeSaleWeb/AmzBL/Products/AmzProductData.cs
@@ -1,9 +1,12 @@
 ﻿using AmzModel;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace AmzBL.Products
 {
@@ -11,19 +14,38 @@ namespace AmzBL.Products
     {
         public static IEnumerable<AmzProduct> GetProducts()
         {
-            List<AmzProduct> result = new List<AmzProduct>();
-            for (int i = 1; i < 80; i++)
+            IEnumerable<AmzProduct> result = new List<AmzProduct>();
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
             {
-                result.Add(new AmzProduct
+                conn.Open();
+                try
                 {
-                    ProductID = i,
-                    ProductName = string.Format("Name {0}", i),
-                    UnitPrice = 10,
-                    UnitsInStock = 50,
-                    UnitsOnOrder = 10,
-                    Discontinued = false
-                });
+                    result = conn.Query<AmzProduct>(@"
+                    SELECT * 
+                    FROM phenix.amz_Products");
+                }
+                catch (Exception ex)
+                {
+                    //PhenixMail.SendMail("daContactDetail.GetClientContactDetail()-ERROR", string.Format("{0}", ex.Message), ConfigurationManager.AppSettings["MAIL_SALES_TEAM"]);
+                }
+
             }
+
+
+
+            //for (int i = 1; i < 80; i++)
+            //{
+            //    result.Add(new AmzProduct
+            //    {
+            //        ProductID = i,
+            //        ProductName = string.Format("Name {0}", i),
+            //        UnitPrice = 10,
+            //        UnitsInStock = 50,
+            //        UnitsOnOrder = 10,
+            //        Discontinued = false
+            //    });
+            //}
 
             return result;
 
