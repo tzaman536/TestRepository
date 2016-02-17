c9b0daa13b8a13070e552b7380cb2b1335659c44
Tanweer.Zaman <tzaman536@gmail.com>
2016-02-17 09:59:55 -0500

    sw

--- AmzWholeSaleWeb/AmzWholeSaleWeb/Controllers/ProductDetailController.cs
+++ AmzWholeSaleWeb/AmzWholeSaleWeb/Controllers/ProductDetailController.cs
@@ -7,6 +7,11 @@ using System.Web.Mvc;
 
 namespace AmzWholeSaleWeb.Controllers
 {
+    public class ProductImage
+    {
+        public string ImageId { get; set; }
+    }
+
     public class ProductDetailController : Controller
     {
         private AmzProductHandler productHandler;
@@ -20,9 +25,44 @@ namespace AmzWholeSaleWeb.Controllers
         public ActionResult Index(int productID)
         {
             var result = productHandler.GetProduct(productID);
+            List<ProductImage> imageList = new List<ProductImage>();
+            if (result != null)
+            {
+                if (!string.IsNullOrEmpty(result.OriginalImageId))
+                {
+                    var img = new ProductImage() { ImageId = result.OriginalImageId };
+                    imageList.Add(img);
+                }
+                if (!string.IsNullOrEmpty(result.ImageIdOne))
+                {
+                    var img = new ProductImage() { ImageId = result.ImageIdOne };
+                    imageList.Add(img);
+                }
+                if (!string.IsNullOrEmpty(result.ImageIdTwo))
+                {
+                    var img = new ProductImage() { ImageId = result.ImageIdTwo };
+                    imageList.Add(img);
+                }
+                if (!string.IsNullOrEmpty(result.ImageIdThree))
+                {
+                    var img = new ProductImage() { ImageId = result.ImageIdThree };
+                    imageList.Add(img);
+                }
+                if (!string.IsNullOrEmpty(result.ImageIdFour))
+                {
+                    var img = new ProductImage() { ImageId = result.ImageIdFour };
+                    imageList.Add(img);
+                }
+                if (!string.IsNullOrEmpty(result.ImageIdFive))
+                {
+                    var img = new ProductImage() { ImageId = result.ImageIdFive };
+                    imageList.Add(img);
+                }
+
+            }
 
             ViewBag.Product = result;
-            return View();
+            return View(imageList);
         }
     }
 }
