54ae5f2a370ac615d97038805f0e0480ac903a64
Tanweer.Zaman <tzaman536@gmail.com>
2016-03-10 05:41:04 -0500

    sw

---
+++ SimplexSysWeb/SimplexSysWeb/Controllers/SimplexSysHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimplexSysWeb.Controllers
+{
+    public class SimplexSysHomeController : Controller
+    {
+        // GET: SimplexSysHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
