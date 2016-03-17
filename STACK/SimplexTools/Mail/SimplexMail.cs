3e64eed7f2022bd279be4ebdbb07c9ecc39119c7
Tanweer.Zaman <tzaman536@gmail.com>
2016-03-17 14:52:48 -0400

    sw

--- STACK/SimplexTools/Mail/SimplexMail.cs
+++ STACK/SimplexTools/Mail/SimplexMail.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Net.Mail;
 using System.Configuration;
 using System.Net;
+using log4net;
 
 namespace PhenixTools.Mail
 {
     public class PhenixMail
     {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(PhenixMail));
 
         public static void SendMail(string subject, string body, string to)
         {
             try
             {
+                logger.Info("Sending mail.....");
                 MailMessage msg = new MailMessage();
                 msg.Subject = subject;
                 msg.From = new MailAddress(ConfigurationManager.AppSettings["MAIL_FROM"]);
@@ -25,12 +28,18 @@ namespace PhenixTools.Mail
                 smtp.Port = int.Parse(ConfigurationManager.AppSettings["SMTP_PORT"]);
                 smtp.UseDefaultCredentials = false;
                 smtp.EnableSsl = true;
+                //SMTP_PORT = 25
+                // send is the password for send@simplessys.co
+                // smtp server name : localhost
+                // userid: send@simplexsys.co
+                //NetworkCredential nc = new NetworkCredential(send@simplexsys.co, "send");
                 NetworkCredential nc = new NetworkCredential(ConfigurationManager.AppSettings["MAIL_FROM"], ConfigurationManager.AppSettings["MAIL_FROM_CRED"]);
                 smtp.Credentials = nc;
                 smtp.Send(msg);
             }
             catch (Exception ex)
             {
+                logger.Fatal(ex);
             }
 
         }
--- SimplexSysWeb/SimplexSysWeb/Global.asax.cs
+++ SimplexSysWeb/SimplexSysWeb/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,16 @@ namespace SimplexSysWeb
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(MvcApplication));
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure();
+
         }
     }
 }
