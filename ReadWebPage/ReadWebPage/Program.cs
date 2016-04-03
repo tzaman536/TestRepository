f8e05e9f08b5b77861731c63e6d1a6b7b253d59e
Tanweer.Zaman <tzaman536@gmail.com>
2016-04-03 11:22:35 -0400

    sw

---
+++ ReadWebPage/ReadWebPage/Program.cs
@@ -0,0 +1,252 @@
+﻿using HtmlAgilityPack;
+using mshtml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace ReadWebPage
+{
+    class Company
+    {
+        public string CompanyName { get; set; }
+        public DateTime FilingDate { get; set; }
+        public string Address { get; set; }
+        public string DOSID { get; set; }
+
+    }
+
+    class Program
+    {
+        static List<Company> companyList = new List<Company>();
+
+        public static void EnrichCompany(string innterText, ref Company c)
+        {
+            try
+            {
+                string[] temp = innterText.Split('\n');
+                if (innterText.Contains("Current Entity Name"))
+                {
+                    foreach (var w in temp)
+                    {
+                        w.Trim();
+
+                        if (string.IsNullOrEmpty(w))
+                            continue;
+                        if (w.Contains("Current Entity Name"))
+                            continue;
+
+                        c.CompanyName = w.Replace("amp;", "");
+                        break;
+                    }
+
+                }
+
+                if (innterText.Contains("Initial DOS Filing Date"))
+                {
+                    DateTime t = new DateTime();
+                    foreach (var w in temp)
+                    {
+                        w.Trim();
+                        if (string.IsNullOrEmpty(w))
+                            continue;
+                        if (w.Contains("Initial DOS Filing Date"))
+                            continue;
+
+                        if (DateTime.TryParse(w, out t))
+                            c.FilingDate = t;
+                        break;
+                    }
+
+                }
+
+
+                if (innterText.Contains("DOS Process"))
+                {
+                    innterText.Replace("DOS Process", "");
+                    innterText.Replace("(Address to which DOS will mail process if accepted on behalf of the entity)", "");
+                    c.CompanyName = innterText;
+                }
+
+                if (innterText.Contains("DOS ID"))
+                {
+                    foreach (var w in temp)
+                    {
+                        w.Trim();
+
+                        if (string.IsNullOrEmpty(w))
+                            continue;
+                        if (w.Contains("DOS ID"))
+                            continue;
+
+                        c.DOSID = w.Replace("amp;", "");
+                        break;
+                    }
+
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        static void ReadCompanyInfo(string href)
+        {
+
+            string url = "https://appext20.dos.ny.gov/corp_public/" + href.Replace("amp;", "");
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    string result = client.DownloadString(url);
+                    HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+
+                    // There are various options, set as needed
+                    htmlDoc.OptionFixNestedTags = true;
+
+                    // filePath is a path to a file containing the html
+                    htmlDoc.LoadHtml(result);
+
+                    // Use:  htmlDoc.LoadHtml(xmlString);  to load from a string (was htmlDoc.LoadXML(xmlString)
+
+
+
+                    // ParseErrors is an ArrayList containing any errors from the Load statement
+                    if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0)
+                    {
+                        // Handle any parse errors as required
+
+                    }
+                    else
+                    {
+
+                        if (htmlDoc.DocumentNode != null)
+                        {
+                            HtmlAgilityPack.HtmlNodeCollection tableNodes = htmlDoc.DocumentNode.SelectNodes("//table");
+                            Company c = new Company();
+
+                            foreach (var node in tableNodes)
+                            {
+                                // Do something with bodyNode
+                                if (node != null)
+                                {
+                                    if (node.InnerText.Contains("Selected Entity Status Information"))
+                                    {
+
+                                        foreach (HtmlNode row in node.SelectNodes("tr"))
+                                        {
+                                            EnrichCompany(row.InnerText, ref c);
+                                            //Console.WriteLine("row");
+                                            //foreach (HtmlNode cell in row.SelectNodes("th|td"))
+                                            //{
+                                            //    Console.WriteLine("cell: " + cell.InnerText);
+                                            //}
+                                        }
+                                    }
+
+                                    if (node.InnerText.Contains("Selected Entity Address Information"))
+                                    {
+                                        foreach (HtmlNode row in node.SelectNodes("tr"))
+                                        {
+                                            if (row.InnerText.Contains("DOS Process"))
+                                                continue;
+
+                                            if (row.InnerText.Contains("Address to which DOS will "))
+                                                continue;
+
+                                            if (row.InnerText.Contains("Chief Executive Officer"))
+                                                break;
+
+                                            c.Address = row.InnerText.Replace("amp;", "");
+                                        }
+                                    }
+
+                                }
+                            }
+                            if (!string.IsNullOrEmpty(c.CompanyName))
+                                companyList.Add(c);
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+
+        static void ProcessPage(string companyStartsWith, string pageNumber)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+
+                    //string result = client.DownloadString("https://appext20.dos.ny.gov/corp_public/CORPSEARCH.SELECT_ENTITY?p_srch_results_page=20&p_entity_name=b&p_name_type=A&p_search_type=BEGINS");
+                    string result = client.DownloadString(string.Format("https://appext20.dos.ny.gov/corp_public/CORPSEARCH.SELECT_ENTITY?p_srch_results_page={0}&p_entity_name={1}&p_name_type=A&p_search_type=BEGINS", pageNumber, companyStartsWith));
+                    // TODO: do something with the downloaded result from the remote
+                    // web site
+                    //Console.WriteLine(result);
+
+                    Match m;
+                    string HRefPattern = "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
+
+                    try
+                    {
+                        m = Regex.Match(result, HRefPattern,
+                                        RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                        while (m.Success)
+                        {
+                            if (m.Groups[1] != null && m.Groups[1].Value.Contains("CORPSEARCH"))
+                            {
+                                Console.WriteLine("Found href " + m.Groups[1] + " at "
+                                   + m.Groups[1].Index);
+
+                                ReadCompanyInfo(m.Groups[1].Value);
+                            }
+                            m = m.NextMatch();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("The matching operation timed out.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //https://appext20.dos.ny.gov/corp_public/CORPSEARCH.SELECT_ENTITY?p_srch_results_page=20&p_entity_name=b&p_name_type=A&p_search_type=BEGINS
+
+
+            for (int i = 1; i < 21; i++)
+            {
+                ProcessPage("AA", i.ToString());
+            }
+
+            var orderByDateList = companyList.OrderByDescending(x => x.FilingDate);
+            foreach (var c in orderByDateList)
+            {
+                Console.WriteLine(string.Format("{0} {1}", c.CompanyName, c.FilingDate));
+            }
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
