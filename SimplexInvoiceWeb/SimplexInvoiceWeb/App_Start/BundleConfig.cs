cc38ef20fa0281a5feca00da57434e4c5eebb0a5
Tanweer.Zaman <tzaman536@gmail.com>
2016-04-26 21:16:52 -0400

    sw

--- SimplexInvoiceWeb/SimplexInvoiceWeb/App_Start/BundleConfig.cs
+++ SimplexInvoiceWeb/SimplexInvoiceWeb/App_Start/BundleConfig.cs
@@ -26,6 +26,20 @@ namespace SimplexInvoiceWeb
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            #region Kendo files
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+            "~/Scripts/kendo/kendo.all.min.js",
+            // "~/Scripts/kendo/kendo.timezones.min.js", // uncomment if using the Scheduler
+            "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+            "~/Content/kendo/kendo.common-bootstrap.min.css",
+            "~/Content/kendo/kendo.bootstrap.min.css"));
+
+            bundles.IgnoreList.Clear();
+            #endregion
+
         }
     }
 }
