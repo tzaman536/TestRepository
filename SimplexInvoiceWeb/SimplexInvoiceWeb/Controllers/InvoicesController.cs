0cbbc1fccfdbcbc6fba428df93f6a82195c583a5
Tanweer.Zaman <tzaman536@gmail.com>
2016-04-27 22:17:44 -0400

    sw

---
+++ SimplexInvoiceWeb/SimplexInvoiceWeb/Controllers/InvoicesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimplexInvoiceWeb.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        // GET: Invoices
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
