db07d9482e8ffe236a1a321c2d3b23f7622bcd1f
Tanweer.Zaman <tzaman536@gmail.com>
2016-04-28 13:39:48 -0400

    sw

--- SimplexInvoiceWeb/SimplexInvoiceWeb/Controllers/MyClientsController.cs
+++ SimplexInvoiceWeb/SimplexInvoiceWeb/Controllers/MyClientsController.cs
@@ -6,12 +6,61 @@ using System.Web.Mvc;
 
 namespace SimplexInvoiceWeb.Controllers
 {
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+    }
     public class MyClientsController : Controller
     {
+
+
+        private Product[] products = {
+                                         new Product {
+                                             ProductID = 1, ProductName = "Audi A3",
+                                             Description = "New Audi A3", Category = "Car",
+                                             Price = 25000
+                                         },
+                                         new Product {
+                                             ProductID = 2, ProductName = "VW Golf",
+                                             Description = "New VW Golf", Category = "Car",
+                                             Price = 22000
+                                         },
+                                         new Product {
+                                             ProductID = 3, ProductName = "Boing 747",
+                                             Description = "The new Boing airplane", Category = "Airplane",
+                                             Price = 2000000
+                                         },
+                                         new Product {
+                                             ProductID = 4, ProductName = "Boing 747",
+                                             Description = "The new Boing airplane", Category = "Airplane",
+                                             Price = 2000000
+                                         },
+                                         new Product {
+                                             ProductID = 5, ProductName = "Yamaha 250",
+                                             Description = "Yamaha's new motorcycle", Category = "Motorcycle",
+                                             Price = 5000
+                                         },
+                                         new Product {
+                                             ProductID = 6, ProductName = "honda 750",
+                                             Description = "Honda's new motorcycle", Category = "Motorcycle",
+                                             Price = 7000
+                                         }
+
+                                     };
         // GET: MyClients
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetProductDataJson()
+        {
+            IEnumerable<Product> data = products.ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
