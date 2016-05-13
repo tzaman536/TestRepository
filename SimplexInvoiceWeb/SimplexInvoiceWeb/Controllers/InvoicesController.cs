10207dee19b513010cc4077f43fc9620a3f2db40
Tanweer.Zaman <tzaman536@gmail.com>
2016-05-12 21:43:28 -0400

    sw

--- SimplexInvoiceWeb/SimplexInvoiceWeb/Controllers/InvoicesController.cs
+++ SimplexInvoiceWeb/SimplexInvoiceWeb/Controllers/InvoicesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using log4net;
+using SimplexInvoiceBL;
+using SimplexInvoiceModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +11,14 @@ namespace SimplexInvoiceWeb.Controllers
 {
     public class InvoicesController : Controller
     {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(MyCompanyController));
+
+
+        SimplexInvoiceHelper helper = new SimplexInvoiceHelper();
+        LogisticsCompanyHandler ch = new LogisticsCompanyHandler();
+        ClientsCompanyHandler clientCompanyHandler = new ClientsCompanyHandler();
+        LogisticsCompany lc;
+
         // GET: Invoices
         public ActionResult Index()
         {
@@ -15,6 +26,37 @@ namespace SimplexInvoiceWeb.Controllers
             {
                 return RedirectToAction("Login", "Account");
             }
+
+            ClientCompany c = null;
+
+            if (c == null)
+            {
+                c = new ClientCompany();
+                c.CompanyName = string.Empty;
+                c.ContactPerson = string.Empty;
+                c.AddressLine1 = string.Empty;
+                c.AddressLine2 = string.Empty;
+                c.City = string.Empty;
+                c.State = "NY";
+                c.Zip = string.Empty;
+                c.Email = string.Empty;
+                c.MobileNumber = string.Empty;
+                c.OfficeNumber = string.Empty;
+                c.FaxNumber = string.Empty;
+            }
+
+            lc = ch.GetCompanyRegisteredByUser(User.Identity.Name);
+            if (lc != null)
+            {
+                c.WeightRate = lc.WeightRate;
+                c.ComplimentaryWeight = lc.ComplimentaryWeight;
+                c.BasePickupCharge = lc.BasePickupCharge;
+            }
+
+
+            ViewData["PU_FORM"] = c;
+
+
 
             return View();
         }
