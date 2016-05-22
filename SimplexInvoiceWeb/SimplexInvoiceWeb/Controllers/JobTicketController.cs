825865e8c2f664ce788ffe3040858bac0d5f18db
Tanweer.Zaman <tzaman536@gmail.com>
2016-05-22 10:45:35 -0400

    sw

--- SimplexInvoiceWeb/SimplexInvoiceWeb/Controllers/JobTicketController.cs
+++ SimplexInvoiceWeb/SimplexInvoiceWeb/Controllers/JobTicketController.cs
@@ -1,17 +1,55 @@
-﻿using System;
+﻿using Kendo.Mvc.UI;
+using SimplexInvoiceBL;
+using SimplexInvoiceModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace SimplexInvoiceWeb.Controllers
 {
     public class JobTicketController : Controller
     {
+        LogisticsCompanyHandler lch = new LogisticsCompanyHandler();
+        ClientsCompanyHandler cch = new ClientsCompanyHandler();
+        LogisticsCompany lc;
+
         // GET: JobTicket
         public ActionResult Index()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
             return View();
         }
+
+        [HttpPost]
+        public ActionResult CalcTotalCharge([DataSourceRequest]DataSourceRequest request, decimal inputQuantity, decimal inputWeight, decimal inputBasePickupCharge, decimal inputMilage
+                                            ,decimal inputToll, decimal inputFuelSurcharge,decimal inputMiscCharges, string inputClient)
+        {
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+
+            if (string.IsNullOrEmpty(inputClient))
+                return Json(new { success = true, message = 0 }, JsonRequestBehavior.AllowGet);
+
+            if (lc == null)
+                lc = lch.GetCompanyRegisteredByUser(User.Identity.Name);
+
+            ClientCompany c = cch.GetCompanyByName(inputClient, lc);
+
+            //decimal totalCharges = inputQuantity * inputWeight * 
+
+            return Json(new { success = true, message = 500 }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
