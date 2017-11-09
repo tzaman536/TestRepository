c2f4c04e52bd400ae95b030987b6c7ae95d68f42
tzaman536@gmail.com <tzaman536@gmail.com>
2017-11-09 09:37:15 -0500

    sw

---
+++ Rnzss/RnzssWeb/Controllers/RfqController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsNetworkWeb.Controllers
+{
+    public class RfqController : Controller
+    {
+        // GET: Rfq
+        public ActionResult RfqEntry()
+        {
+            return View();
+        }
+    }
+}
