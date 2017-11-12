7e4cff9cb9fe9bacb7e757ba0fea9d3f1e8f6249
tzaman536@gmail.com <tzaman536@gmail.com>
2017-11-12 14:33:34 -0500

    sw

--- Rnzss/RnzssWeb/Controllers/RfqController.cs
+++ Rnzss/RnzssWeb/Controllers/RfqController.cs
@@ -716,6 +716,34 @@ namespace RnzssWeb.Controllers
             return RedirectToAction("AddSolicitation");
         }
 
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (HttpPostedFileBase file in files)
+            {
+                //string filePath = Path.Combine(TempPath, file.FileName);
+                //System.IO.File.WriteAllBytes(filePath, ReadData(file.InputStream));
+            }
+
+            return Json("All files have been successfully stored.");
+        }
+
+        private byte[] ReadData(Stream stream)
+        {
+            byte[] buffer = new byte[16 * 1024];
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+
+                return ms.ToArray();
+            }
+        }
+
         #endregion
 
 
