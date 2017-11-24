686dd5d4103244d58c7c6f678442b5d598a130a6
tzaman536@gmail.com <tzaman536@gmail.com>
2017-11-24 15:43:06 -0500

    sw

--- Rnzss/Sql/Working.sql
+++ Rnzss/Sql/Working.sql
@@ -1,16 +1,22 @@
 use rnzss
-declare @RfqNo nvarchar(200) ='RZRFQ10116'
+
+declare @RfqNo nvarchar(200) ='RZRFQ10124'
 
 
 select * from [rnz].[ProductInformation] WHERE RFQNo=@RfqNo
-select * from rnz.RequestForQuoteEvents  WHERE RFQNo=@RfqNo
 select * from [rnz].[RequestForQuote] WHERE RFQNo=@RfqNo
+select * from rnz.RequestForQuoteEvents  WHERE RFQNo=@RfqNo
 select * from rnz.DocumentStore WHERE LinkId=@RfqNo
 
 
 select * from [rnz].[Vendors]
 
-select * from [rnz].[RequestForQuote] order by 1 desc
+select * 
+from [rnz].[RequestForQuote] 
+where RfqStatus = 'Closed'
+order by 1 desc
+
+
 select * from [rnz].Solicitations
 select * from rnz.DocumentStore
 
