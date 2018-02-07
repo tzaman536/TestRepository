cf0196bd460e39a62a4982ee8e5e2b14b2da271b
Zaman, Tanweer <Tanweer.Zaman@prcm.com>
2018-02-07 14:17:15 -0500

    sw

--- BondCalculator/BondCalculator/BondCalculatorCalc/Calculator.cs
+++ BondCalculator/BondCalculator/BondCalculatorCalc/Calculator.cs
@@ -10,12 +10,91 @@ namespace BondCalculatorCalc
     {
         public double CalcPrice(double coupon, int years, double face, double rate)
         {
-            return 0;
+            
+            double presentValueOfCashFlow = 0;
+            double presentValueOfFaceAmount = 0;
+
+            try {
+                double c = (face * coupon) / 2;
+                double n = years * 2;
+                double i = rate / 2;
+                
+                presentValueOfCashFlow = c * (((1 - (1 / Math.Pow(1 + i, n))) / i));
+                presentValueOfFaceAmount = face * (1 / Math.Pow(1 + i, n));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+            return Math.Round(presentValueOfCashFlow + presentValueOfFaceAmount,7);
         }
 
 
-        public double CalcYield(double coupon, int years, double face, double rate)
+        public double CalcYield(double coupon, int years, double face, double price)
         {
+            double presentValueOfCashFlow = 0;
+            double presentValueOfFaceAmount = 0;
+
+            try
+            {
+                double c = (face * coupon) / 2;
+                double n = years * 2;
+                double i = coupon / 2;
+
+                presentValueOfCashFlow = c * (((1 - (1 / Math.Pow(1 + i, n))) / i));
+                presentValueOfFaceAmount = face * (1 / Math.Pow(1 + i, n));
+
+                double tempPrice = presentValueOfCashFlow + presentValueOfFaceAmount;
+                double factor = .005;
+                string factorDirection = string.Empty;
+
+                Dictionary<double, string> visitedPrice = new Dictionary<double, string>();
+
+                int attemptCount = 0; 
+                while (Math.Round(tempPrice) != Math.Round(price))
+                {
+                    attemptCount++;
+                    Console.WriteLine("Attempt {0} to find YTM for price {1}", attemptCount, price );
+                    if(!visitedPrice.ContainsKey(tempPrice))
+                    {
+                        visitedPrice.Add(tempPrice,null);
+                    }
+                    else
+                    {
+                        factor = factor - .0010;
+
+                        if (factor <= 0)
+                            factor = .0001;
+                    }
+
+                    if (tempPrice > price)
+                    {
+                        coupon = coupon + factor;
+                    }
+                    else
+                    {
+                        coupon = coupon - factor;
+                    }
+
+                    n = years * 2;
+                    i = coupon / 2;
+                    presentValueOfCashFlow = c * (((1 - (1 / Math.Pow(1 + i, n))) / i));
+                    presentValueOfFaceAmount = face * (1 / Math.Pow(1 + i, n));
+                    tempPrice = presentValueOfCashFlow + presentValueOfFaceAmount;
+                }
+
+                return coupon;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+
             return 0;
         }
     }
--- BondCalculator/BondCalculator/BondCalculatorConsole/Program.cs
+++ BondCalculator/BondCalculator/BondCalculatorConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BondCalculator;
+using BondCalculatorCalc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,22 @@ namespace BondCalculatorConsole
     {
         static void Main(string[] args)
         {
-            InputBond b = new InputBond();
+            Calculator c = new Calculator();
+            /*double priceDiscountTest = c.CalcPrice(0.1, 5, 1000, .15);
+            double priceDiscountFib = c.CalcPrice(0.09, 20, 1000, .12);
+            double pricePermium = c.CalcPrice(0.1, 5, 1000, .08);
+            */
+            
+            double yieldDiscount = Math.Round(c.CalcYield(0.06, 18, 1000, 700.89),7);
+            double priceWithYieldDiscount = Math.Round(c.CalcPrice(0.06, 18, 1000, yieldDiscount),2);
+            
+
+            double yieldPremium = Math.Round(c.CalcYield(0.06, 18, 1000, 1200),7);
+            double priceWithYieldPermium = Math.Round(c.CalcPrice(0.06, 18, 1000, yieldPremium),2);
+
+            Console.WriteLine("Presess any key to continue");
+            Console.ReadLine();
+
         }
     }
 }
