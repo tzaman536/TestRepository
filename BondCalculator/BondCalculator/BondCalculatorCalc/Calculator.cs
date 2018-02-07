28bc1c0a385b448cdda9f03f6e1afb23606cb687
Zaman, Tanweer <Tanweer.Zaman@prcm.com>
2018-02-07 15:09:38 -0500

    sw

--- BondCalculator/BondCalculator/BondCalculatorCalc/Calculator.cs
+++ BondCalculator/BondCalculator/BondCalculatorCalc/Calculator.cs
@@ -6,18 +6,38 @@ using System.Threading.Tasks;
 
 namespace BondCalculatorCalc
 {
+
+    public enum PaymentFrequency
+    {
+        Semiannually
+        ,Quarterly
+        ,Annually
+    }
     public class Calculator
     {
-        public double CalcPrice(double coupon, int years, double face, double rate)
+        public event CalculatorSendMessageHandler Message;
+
+        public delegate void CalculatorSendMessageHandler(string message);
+
+        public double CalcPrice(double coupon, int years, double face, double rate, PaymentFrequency frequency = PaymentFrequency.Semiannually)
         {
-            
+
+
             double presentValueOfCashFlow = 0;
             double presentValueOfFaceAmount = 0;
 
             try {
-                double c = (face * coupon) / 2;
-                double n = years * 2;
-                double i = rate / 2;
+
+                int paymentFrequency = 2;
+                if (frequency == PaymentFrequency.Quarterly)
+                    paymentFrequency = 4;
+                if (frequency == PaymentFrequency.Annually)
+                    paymentFrequency = 1;
+
+
+                double c = (face * coupon) / paymentFrequency;
+                double n = years * paymentFrequency;
+                double i = rate / paymentFrequency;
                 
                 presentValueOfCashFlow = c * (((1 - (1 / Math.Pow(1 + i, n))) / i));
                 presentValueOfFaceAmount = face * (1 / Math.Pow(1 + i, n));
@@ -32,16 +52,24 @@ namespace BondCalculatorCalc
         }
 
 
-        public double CalcYield(double coupon, int years, double face, double price)
+        public double CalcYield(double coupon, int years, double face, double price, PaymentFrequency frequency = PaymentFrequency.Semiannually)
         {
             double presentValueOfCashFlow = 0;
             double presentValueOfFaceAmount = 0;
 
             try
             {
-                double c = (face * coupon) / 2;
-                double n = years * 2;
-                double i = coupon / 2;
+                int paymentFrequency = 2;
+
+                if (frequency == PaymentFrequency.Quarterly)
+                    paymentFrequency = 4;
+                if (frequency == PaymentFrequency.Annually)
+                    paymentFrequency = 1;
+
+
+                double c = (face * coupon) / paymentFrequency;
+                double n = years * paymentFrequency;
+                double i = coupon / paymentFrequency;
 
                 presentValueOfCashFlow = c * (((1 - (1 / Math.Pow(1 + i, n))) / i));
                 presentValueOfFaceAmount = face * (1 / Math.Pow(1 + i, n));
@@ -56,13 +84,18 @@ namespace BondCalculatorCalc
                 while (Math.Round(tempPrice) != Math.Round(price))
                 {
                     attemptCount++;
-                    Console.WriteLine("Attempt {0} to find YTM for price {1}", attemptCount, price );
-                    if(!visitedPrice.ContainsKey(tempPrice))
+                    if(Message != null)
+                    {
+                        Message(string.Format("Attempt {0} to find YTM for price {1}", attemptCount, price));
+                    }
+
+                    if (!visitedPrice.ContainsKey(tempPrice))
                     {
                         visitedPrice.Add(tempPrice,null);
                     }
                     else
                     {
+                        // We will keep adjusting discount factor to get closer to the price
                         factor = factor - .0010;
 
                         if (factor <= 0)
@@ -78,8 +111,8 @@ namespace BondCalculatorCalc
                         coupon = coupon - factor;
                     }
 
-                    n = years * 2;
-                    i = coupon / 2;
+                    n = years * paymentFrequency;
+                    i = coupon / paymentFrequency;
                     presentValueOfCashFlow = c * (((1 - (1 / Math.Pow(1 + i, n))) / i));
                     presentValueOfFaceAmount = face * (1 / Math.Pow(1 + i, n));
                     tempPrice = presentValueOfCashFlow + presentValueOfFaceAmount;
--- BondCalculator/BondCalculator/BondCalculatorConsole/Program.cs
+++ BondCalculator/BondCalculator/BondCalculatorConsole/Program.cs
@@ -13,6 +13,7 @@ namespace BondCalculatorConsole
         static void Main(string[] args)
         {
             Calculator c = new Calculator();
+            c.Message += C_Message;
             //returns 528.8807463
             double price = Math.Round(c.CalcPrice(0.10, 30, 1000, 0.19),7);
             // returns 0.1900000
@@ -26,9 +27,17 @@ namespace BondCalculatorConsole
             double yieldPremium = Math.Round(c.CalcYield(0.06, 18, 1000, 1200),7);
             double priceWithYieldPermium = Math.Round(c.CalcPrice(0.06, 18, 1000, yieldPremium),2);
             */
+
+
             Console.WriteLine("Presess any key to continue");
             Console.ReadLine();
 
         }
+
+        private static void C_Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
     }
 }
