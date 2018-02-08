80a9ad0e8454b0949962d79ded6e414d208fe16a
Zaman, Tanweer <Tanweer.Zaman@prcm.com>
2018-02-08 13:37:31 -0500

    sw

---
+++ BondCalculator/BondCalculator/Validators.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BondCalculator
+{
+    public class NameValidator : ValidationRule
+    {
+        public override ValidationResult Validate
+          (object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            try {
+                if (value == null)
+                    return new ValidationResult(false, "value cannot be empty.");
+                else
+                {
+                    if (String.IsNullOrEmpty(value.ToString()))
+                        return new ValidationResult
+                        (false, "value can not be empty.");
+                }
+                return ValidationResult.ValidResult;
+            }
+            catch(Exception ex)
+            {
+                return new ValidationResult
+                (false, ex.Message);
+            }
+        }
+    }
+}
