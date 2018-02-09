06de84bee417081af52f34b15b114d953d4a7a88
Zaman, Tanweer <Tanweer.Zaman@prcm.com>
2018-02-09 15:16:01 -0500

    sw

--- BondCalculator/BondCalculator/BondCalculatorConsole/Program.cs
+++ BondCalculator/BondCalculator/BondCalculatorConsole/Program.cs
@@ -12,26 +12,131 @@ namespace BondCalculatorConsole
     {
         static void Main(string[] args)
         {
-            Calculator c = new Calculator();
-            c.Message += C_Message;
-            //returns 528.8807463
-            double price = Math.Round(c.CalcPrice(0.10, 30, 1000, 0.19),7);
-            // returns 0.1900000
-            double ytm = Math.Round(c.CalcYield(0.10, 30, 1000, price),2); 
+            double tempInputDouble;
+            int tempInputInt;
+            int runtType;
+            string userInput = "xyz";
+            Console.WriteLine("Type exit to close the console");
 
-            /*
-            double yieldDiscount = Math.Round(c.CalcYield(0.06, 18, 1000, 700.89),7);
-            double priceWithYieldDiscount = Math.Round(c.CalcPrice(0.06, 18, 1000, yieldDiscount),2);
-            
+            while (!userInput.ToLower().Equals("exit"))
+            {
+                Console.WriteLine();
+                Console.Write("Enter 1 to compute price 2 to compute yield to maturity: ");
+                userInput = Console.ReadLine();
 
-            double yieldPremium = Math.Round(c.CalcYield(0.06, 18, 1000, 1200),7);
-            double priceWithYieldPermium = Math.Round(c.CalcPrice(0.06, 18, 1000, yieldPremium),2);
-            */
+                if (userInput.ToLower().Equals("exit"))
+                    break;
+
+                double coupon;
+                int years;
+                double face;
+                double rate;
+                double price;
 
 
+                if (int.TryParse(userInput, out runtType))
+                {
+                    try
+                    {
+                        if (runtType > 2 || runtType < 1)
+                        {
+                            throw new Exception("Please enter either 1 or 2");
+                        }
+
+                        #region Face
+                        Console.Write("Enter Face Amount: ");
+                        userInput = Console.ReadLine();
+                        if (double.TryParse(userInput, out tempInputDouble))
+                        {
+                            face = tempInputDouble;
+                        }
+                        else
+                        {
+                            throw new Exception("Please enter a valid face amount.");
+                        }
+                        #endregion
+
+                        #region Years to maturity
+                        Console.Write("Enter Years To Maturity: ");
+                        userInput = Console.ReadLine();
+                        if (int.TryParse(userInput, out tempInputInt))
+                        {
+                            years = tempInputInt;
+                        }
+                        else
+                        {
+                            throw new Exception("Please enter a valid years to maturity.");
+                        }
+                        #endregion
+
+                        #region Coupon
+                        Console.Write("Enter Coupon(%): ");
+                        userInput = Console.ReadLine();
+                        if (double.TryParse(userInput, out tempInputDouble))
+                        {
+                            coupon = tempInputDouble;
+                        }
+                        else
+                        {
+                            throw new Exception("Please enter a valid coupon.");
+                        }
+                        #endregion
+
+
+                        Calculator calculator = new Calculator();
+                        calculator.Message += Calculator_Message;
+
+                        if (runtType == 1)
+                        {
+                            Console.Write("Enter Discount Rate(%): ");
+                            userInput = Console.ReadLine();
+                            if (double.TryParse(userInput, out tempInputDouble))
+                            {
+                                rate = tempInputDouble;
+                            }
+                            else
+                            {
+                                throw new Exception("Please enter a valid discount rate.");
+                            }
+
+                            calculator.CalcPrice(coupon, years, face, rate);
+                        }
+
+                        if (runtType == 2)
+                        {
+                            Console.Write("Enter Price: ");
+                            userInput = Console.ReadLine();
+                            if (double.TryParse(userInput, out tempInputDouble))
+                            {
+                                price = tempInputDouble;
+                            }
+                            else
+                            {
+                                throw new Exception("Please enter a valid price");
+                            }
+
+                            calculator.CalcYield(coupon, years, face, price);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+
+            }
             Console.WriteLine("Presess any key to continue");
             Console.ReadLine();
 
+        }
+
+        private static void Calculator_Message(string message)
+        {
+            Console.WriteLine(message);
         }
 
         private static void C_Message(string message)
